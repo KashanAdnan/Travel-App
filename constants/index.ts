0f803d9712b8e5a768ed5d4c9b6cb4ff018dfcba
Kashan Adnan <khanzaidaboy@gmail.com>
2023-10-23 23:13:26 -0700

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -4,7 +4,7 @@ export const NAV_LINKS = [
     { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
     { href: '/', key: 'services', label: 'Services' },
     { href: '/', key: 'pricing ', label: 'Pricing ' },
-    { href: '/', key: 'contact_us', label: 'Contact Us' },
+    { href: '/', key: 'contact_us', label: 'Csdasdsaontact Us' },
   ];
   
   // CAMP SECTION
